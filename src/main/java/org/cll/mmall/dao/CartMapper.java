9f905778e415c63e1111898785965c0eb70dd9a0
ChenLiangliang <chenliangliang2016@gmail.com>
2018-06-11 11:19:45 +0800

    init

---
+++ src/main/java/org/cll/mmall/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package org.cll.mmall.dao;
+
+import org.cll.mmall.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
